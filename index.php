fc690a71aef0b805d380b89d9726427482bd4c92
Ramesh Kumar manjhi <99356690+rdmm1010@users.noreply.github.com>
2024-07-15 17:27:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-<body bgcolour='aqua'>
-<?php
-print "i am ramesh\n";
-print "new feature\n <br />";
-?>  
-<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body bgcolor="aqua">
+    <?php
+    print "I am Ramesh\n";
+    print "New feature<br />";
+    ?>  
+</body>
+</html>
+
