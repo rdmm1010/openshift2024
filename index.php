cd3bd4ee2936d925e562e4d10f793908c18ddba1
Ramesh Kumar manjhi <99356690+rdmm1010@users.noreply.github.com>
2024-07-15 18:45:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body bgcolor="aqua">
-    <?php
-    print "I am Ramesh\n";
-    print "New feature<br />";
-    ?>  
-</body>
-</html>
+<body bgcolor='red'>
 
+<?php
+    print "I am Ramesh\n";
+    print "Welcome to Openshift DO280<br />";
+
+    ?>
+<body>
