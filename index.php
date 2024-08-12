57d57c260e2161ae9c9ba8f56993cf1ebcb769c1
Ramesh Kumar manjhi <99356690+rdmm1010@users.noreply.github.com>
2024-08-12 17:41:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php
     print "I am Ramesh\n";
     print "Welcome to Openshift DO280<br />";
+    print "I am going to plan DO380<br />";
     print "I have certified openshift exam DO280 from 300 marks<br />";
     ?>
 <body>
